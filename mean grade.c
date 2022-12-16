09ff1a33a0f747790b4ddb6188d2f61aac53c9ee
rromero2112 <116103974+rromero2112@users.noreply.github.com>
2022-12-16 16:09:08 -0500

    Update mean grade.c

--- mean grade.c
+++ mean grade.c
@@ -5,27 +5,30 @@
 int main ()
 
 {
-    int gradeA, gradeB, gradeC, gradeD, gradeE, gradeTotal;
+    int gradeA, gradeB, gradeC, gradeD, gradeE, gradeTotal;   //variables are stated for the following inputs
 
-    printf("Please input five number for grades: ");
-    scanf("%d",&gradeA);
+    printf("Please input five number for grades: ");   //prompt for five numerical grades
+    scanf("%d",&gradeA);   //stores each number entered into specific memory
     scanf("%d",&gradeB);
     scanf("%d",&gradeC);
     scanf("%d",&gradeD);
     scanf("%d",&gradeE);
 
-    gradeTotal=gradeA+gradeB+gradeC+gradeD+gradeE;
+    gradeTotal=gradeA+gradeB+gradeC+gradeD+gradeE;   //calculates the total grade of the five numbers
 
-    int meanGrade;
-    meanGrade = (gradeTotal)/5;
+    int meanGrade;   //variable meanGrade is stated
+    meanGrade = (gradeTotal)/5;   //calculations for mean grade
 
-	printf("Your average is: %d\n", meanGrade);
+	printf("Your average is: %d\n", meanGrade);   //output statement with the mean grade
 	
-    if (meanGrade > 75);
+    if (meanGrade >= 75)   //if statement reads value is greater than 75
     {
-        printf("You passed the class!");
+        printf("You passed the class!");   //prompt for TRUE if statement
     }
-
+    else {
+	    printf("Failed\n");   //else prompt when if statement is FALSE
+	    printf("Sorry, you have to repeat the course");   //prompt after inital else statement
+	
     return 0;
+	}
 }
-
